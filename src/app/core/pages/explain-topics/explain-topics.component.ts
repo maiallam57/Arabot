c4cfaf6526c4de126149437d0201156f7d6c8c8d
mai allam <maimallam57@gmail.com>
2024-09-24 22:25:57 +0300

    components

---
+++ src/app/core/pages/explain-topics/explain-topics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explain-topics',
+  standalone: true,
+  imports: [],
+  templateUrl: './explain-topics.component.html',
+  styleUrl: './explain-topics.component.css'
+})
+export class ExplainTopicsComponent {
+
+}
