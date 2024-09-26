f1bdab7078ccb23888818f2f0d6bb7b3c14ff953
mai allam <maimallam57@gmail.com>
2024-09-26 23:48:34 +0300

    navbarUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/Components/navbar/navbar.component";
+import { FooterComponent } from "./shared/Components/footer/footer.component";
+import { FlowbiteService } from './shared/services/flowbite/flowbite.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Arabot';
+  constructor(private _flowbiteService: FlowbiteService) { }
+
+  ngOnInit(): void {
+    this._flowbiteService.loadFlowbite(flowbite => {
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './core/pages/overview/overview.component';
+import { RegisterComponent } from './core/pages/register/register.component';
+import { NotfoundComponent } from './core/pages/notfound/notfound.component';
+import { TeamComponent } from './core/pages/team/team.component';
+import { ResourcesComponent } from './core/pages/resources/resources.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "overview", pathMatch: "full" },
+    { path: "overview", component: OverviewComponent, title: "Overview" },
+    { path: "register", component: RegisterComponent, title: "Register" },
+    { path: "team", component: TeamComponent, title: "Team" },
+    { path: "resources", component: ResourcesComponent, title: "Resources" },
+
+
+    { path: "**", component: NotfoundComponent, title: "Not Found" }
+
+];
