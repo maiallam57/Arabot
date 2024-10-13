aac8e8330dc2338f3adcad054f4a02e62abe7e43
mai allam <maimallam57@gmail.com>
2024-10-13 15:38:58 +0300

    Chat with your Book

--- src/app/features/pages/chat-with-your-book/chat-with-your-book.component.ts
+++ src/app/features/pages/chat-with-your-book/chat-with-your-book.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../../shared/Components/UI/title/title.component";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-with-your-book',
@@ -11,4 +12,38 @@ import { TitleComponent } from "../../../shared/Components/UI/title/title.compon
 export class ChatWithYourBookComponent {
   title: string = "Chat with your Book";
   desc: string = "Ask about a specific book of your choice";
+
+
+  uploadForm: FormGroup;
+  file!: any;
+  responseList: any = [];
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.uploadForm = this._formBuilder.group({
+      file: [null]
+    });
+  }
+
+  onFileChange(event: any) {
+    this.file = event.target.files[0];
+    if (this.file) {
+      this.uploadForm.patchValue({
+        file: this.file
+      });
+      this.onSubmit();
+    }
+  }
+
+  onSubmit() {
+    console.log('onSubmit', this.uploadForm);
+
+    if (this.uploadForm.valid) {
+      const formData = new FormData();
+      formData.append('file', this.uploadForm.get('file')?.value);
+
+      //API Call
+    }
+  }
+
+
 }
