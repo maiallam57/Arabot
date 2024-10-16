8ee1b312f941abf73ac67bff749bdd7dd6af1275
mai allam <maimallam57@gmail.com>
2024-10-16 09:06:35 +0300

    NotFoundComponent

--- src/app/features/pages/learn-arabic/learn-arabic.component.ts
+++ src/app/features/pages/learn-arabic/learn-arabic.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../../shared/Components/UI/title/title.component";
+import { CardItemComponent } from "../../../shared/Components/card-item/card-item.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-learn-arabic',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [RouterLink, TitleComponent, CardItemComponent],
   templateUrl: './learn-arabic.component.html',
   styleUrl: './learn-arabic.component.css'
 })
