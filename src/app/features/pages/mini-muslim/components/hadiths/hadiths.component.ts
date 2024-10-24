261a88dcd84017318523670215b42c6f6dab8956
mai allam <maimallam57@gmail.com>
2024-10-24 22:43:05 +0300

    hadiths

--- src/app/features/pages/mini-muslim/components/hadiths/hadiths.component.ts
+++ src/app/features/pages/mini-muslim/components/hadiths/hadiths.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TitleComponent } from "../../../../../shared/Components/UI/title/title.component";
+import { NgClass, NgFor } from '@angular/common';
+import { MiniMuslimService } from '../../../../services/mini-muslim.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hadiths',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, NgFor, NgClass],
   templateUrl: './hadiths.component.html',
   styleUrl: './hadiths.component.css'
 })
 export class HadithsComponent {
-  title: string = "Hadiths";
-  desc: string = "";
+  title: string = 'Hadiths';
+  desc: string = '';
 
+  private readonly _miniMuslimService = inject(MiniMuslimService);
+  getHolyQuranSub!: Subscription;
+  hadithData: any = {};
+
+  ngOnInit(): void {
+    this.fetchHadiths(); // Fetch initial hadith data
+  }
+
+  ngOnDestroy(): void {
+    this.getHolyQuranSub?.unsubscribe();
+  }
+
+  fetchHadiths(page: number = 1, limit: number = 10): void {
+    this.getHolyQuranSub = this._miniMuslimService.getHadiths(page.toString(), limit.toString()).subscribe({
+      next: (res) => {
+        this.hadithData = res;
+        console.log(this.hadithData);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  // Navigation methods
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.hadithData.pagination.totalPages) {
+      this.fetchHadiths(page); // Fetch data for the selected page
+    }
+  }
+
+  isCurrentPage(page: number): boolean {
+    return this.hadithData.pagination.currentPage === page;
+  }
 }
